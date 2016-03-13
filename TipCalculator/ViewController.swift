ae623876825b925b33b754ba9986dd3d9b7cd023
asatchison <asatchison@outlook.com>
2016-03-13 12:37:03 -0700

    Uploading final calculator

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ratingSlider: UISlider!
+    @IBOutlet weak var billTB: UITextField!
+    @IBOutlet weak var tipTB: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        ratingSlider.value = 5
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,67 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func ratingBarChanged(sender: UISlider) {
+        sender.value = sender.value.roundUp
+    }
+    @IBAction func calculateTip(sender: UIButton) {
+        calc(billTB.text!.floatValue, ratingAmount:  ratingSlider.value)
+        
+    }
+    
+    func calc(billAmount : Float = 0.0, ratingAmount : Float)
+    {
+        var tip : Float = 0.0
+        if(ratingAmount <= 3)
+        {
+            tip = (billAmount*10)/100
+        }
+        else if(ratingAmount <= 5)
+        {
+            tip = (billAmount*13)/100
+        }
+        else if(ratingAmount <= 7)
+        {
+            tip = (billAmount*15)/100
+        }
+        else if(ratingAmount <= 9)
+        {
+            tip = (billAmount*20)/100
+        }
+        else
+        {
+            tip = (billAmount*25)/100
+        }
+        let stringTip : String = String(format: "%.2f", tip)
+        tipTB.text = "$\(stringTip)"
+    }
+    
+    
 
+}
 
+extension String {
+    var floatValue: Float {
+        return (self as NSString).floatValue
+    }
+}
+
+extension Float {
+    var roundUp : Float {
+        if(self - Float(Int(self)) >= 0.5)
+        {
+            return Float(Int(self))+1
+        }
+        return Float(Int(self))
+    }
+    
+    var currencyFormat : Float{
+        var tempval : Float = self*100
+        tempval -= (tempval - Float(Int(tempval)))
+        print("currency: \(tempval)")
+        tempval = tempval/100
+        return tempval/100
+    }
 }
 
